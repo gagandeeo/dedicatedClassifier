1c11e13abb6bd945ab70deda8118be3e0d960e34
gagandeep-tiger <gagandeep.prasad@tigeranalyitcs.com>
2022-09-19 16:25:39 +0530

    before-eject

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import UploadPage from './pages/UploadPage';
 
 function App() {
+  // const [result, setResult] = useState(false);
+  const result = false;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {result? (
+        <h1>ResultPage</h1>
+      ):(
+        <UploadPage/>
+      )}
     </div>
   );
 }
---
+++ src/pages/UploadPage.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react'
+import * as tf from '@tensorflow/tfjs';
+import { MODEL_CLASSES } from '../model/classes.js';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import "./css/UploadPage.css";
+
+
+const MODEL_PATH = "/model/model.json";
+const IMAGE_SIZE = 224;
+const TOPK_PREDICTIONS = 5;
+
+
+const UploadPage = () => {
+
+    const [file, setFile] = useState(null);
+    const [model, setModel] = useState(null);
+    const [results, setResults] = useState(null);
+
+    useEffect(() => {
+        const fetchModel = async() => {
+            let model = await tf.loadLayersModel(MODEL_PATH);
+            let prediction = tf.tidy(() => model.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3]))) 
+            console.log(prediction);
+            prediction.dispose();
+            setModel(model);
+        }
+        fetchModel().catch(console.error)
+    },[])
+
+
+    const handleImageChange = (e) => {
+        setFile(URL.createObjectURL(e.target.files[0]))
+    }
+
+    const getTopKClasses = (values, topK) => {
+        const valuesAndIndices = [];
+            for (let i = 0; i < values.length; i++) {
+                valuesAndIndices.push({value: values[i], index: i});
+            }
+            valuesAndIndices.sort((a, b) => {
+            return b.value - a.value;
+        });
+        
+        const topkValues = new Float32Array(topK);
+        const topkIndices = new Int32Array(topK);
+        for (let i = 0; i < topK; i++) {
+            topkValues[i] = valuesAndIndices[i].value;
+            topkIndices[i] = valuesAndIndices[i].index;
+        }
+
+        const topClassesAndProbs = [];
+        for (let i = 0; i < topkIndices.length; i++) {
+            topClassesAndProbs.push({
+                className: MODEL_CLASSES[topkIndices[i]],
+                probability: (topkValues[i] * 100).toFixed(2)
+            });
+        }
+        return topClassesAndProbs;
+    }
+
+    const convertToTensor = async() => {
+        const img = new Image()
+        img.src = file
+        await img.decode()
+        return tf.tidy(()=> tf.browser.fromPixels(img).toFloat());
+    }
+
+    const processImage = async () => {
+        const tensor = await convertToTensor()
+        const resizeImage = tf.image.resizeBilinear(tensor, [IMAGE_SIZE, IMAGE_SIZE]);
+        const imageData = tf.tidy(() => resizeImage.expandDims(0).toFloat().div(127).sub(1));
+        return imageData
+    }
+
+    const handlePredict = async () => {
+        const imageData = await processImage()
+        const probabilities =  await model.predict(imageData).data() ;
+        const preds =  getTopKClasses(probabilities, TOPK_PREDICTIONS);
+        console.log(preds);
+        setResults(preds)
+    }
+
+  return (
+    <div className='upload__container'>
+        <Card className='upload__card'>
+            {!file?
+            <IconButton size='large' color="primary" aria-label="upload picture" component="label">
+                <CardContent style={{width:100}} > 
+                    <input hidden accept="image/*" type="file" onChange={handleImageChange} />
+                    <PhotoCamera />
+                </CardContent>
+            </IconButton>
+            :
+            <>
+            <CardContent style={{width:200}}>
+                <img id="image_up" className="upload__image" src={file} />
+                {/* {tensor?<p>{tensor}</p>:null} */}
+            </CardContent>
+            <IconButton size='small'className='upload__button' color="primary" aria-label="upload picture" component="label">
+                    <input hidden accept="image/*" type="file" onChange={handleImageChange} />
+                    <PhotoCamera />
+            </IconButton>
+            </>}
+        </Card>
+        <Button className="result__button"variant="outlined" onClick={handlePredict}>Predict</Button>
+    </div>
+  )
+}
+
+export default UploadPage
